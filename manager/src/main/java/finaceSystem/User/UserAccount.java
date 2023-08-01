c0e51d2bfd5a8ba3d89bdd7cd5cd2b0231a11574
Sree Santhikrishna S <113964005+Santhikrishna26@users.noreply.github.com>
2023-08-01 16:35:11 +0530

    Add files via upload

---
+++ manager/src/main/java/finaceSystem/User/UserAccount.java
@@ -0,0 +1,30 @@
+package finaceSystem.User;
+public abstract class UserAccount {
+    private String username;
+    private String password;
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserAccount(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	// Abstract method to be implemented by subclasses (User and Admin)
+    public abstract boolean isAdmin();
+}
+
